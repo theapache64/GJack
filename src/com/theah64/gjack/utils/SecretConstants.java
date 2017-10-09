bcf83bd68b7e50c52c715fedb8920d10a506aec0
theapache64-charlie <theapache64@gmail.com>
2017-10-09 15:40:03 +0530

    .gitignore refreshed

--- src/com/theah64/gjack/utils/SecretConstants.java
+++
@@ -1,9 +0,0 @@
-package com.theah64.gjack.utils;
-
-/**
- * Created by theapache64 on 9/10/17.
- */
-public class SecretConstants {
-    public static final String GMAIL_USERNAME = "mymailer64@gmail.com";
-    public static final String GMAIL_PASSWORD = "mypassword64";
-}
